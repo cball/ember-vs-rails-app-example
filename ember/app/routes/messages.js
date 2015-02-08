32e58ae2a8a3b9f56f3b7dddc57d0fba07af8b9e
Chris Ball <chris@echobind.com>
2015-02-08 14:04:08 -0500

    Messages are fetched on first load, won't need to fetch again.

--- ember/app/routes/messages.js
+++ ember/app/routes/messages.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var existingMessages = this.store.all('message');
-
-    if (Ember.isEmpty(existingMessages)) {
-      var user = this.modelFor('application');
-      existingMessages = user.get('messages');
-    }
-    return existingMessages;
+    return this.store.all('message');
   }
 });
